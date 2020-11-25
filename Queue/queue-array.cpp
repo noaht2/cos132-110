6bd7abff72f8c43f2643aefce97deec508c803fd
Michael Ross Tarr <mrmrmtarr@gmail.com>
2020-11-25 11:33:59 +0200

    Update queue-array.cpp

--- Queue/queue-array.cpp
+++ Queue/queue-array.cpp
@@ -5,30 +5,28 @@ using namespace std;
 int A[SIZE];
 int front = -1;
 int rear = -1;
+
 //function to check if queue is empty
-bool isempty()
-{
+bool isempty() {
  if(front == -1 && rear == -1)
  return true;
  else
  return false;
 }
 //function to insert element in queue
-void enqueue ( int value )
-{
+void enqueue ( int value ) {
  if (rear == SIZE-1)
   cout<<"Queue is full \n";
- else
- {
+ else {
   if( front == -1)
    front = 0;
   rear++;
   A[rear] = value;
  }
 }
+
 //function to remove element from queue
-void dequeue ( )
-{
+void dequeue () {
  if( isempty() )
   cout<<"Queue is empty\n";
  else
@@ -37,29 +35,27 @@ void dequeue ( )
  else
   front++;
 }
+
 //function to display element at front
-void showfront( )
-{
+void showfront() {
  if( isempty())
   cout<<"Queue is empty\n";
  else
   cout<<"element at front is:"<<A[front]<<"\n";
 }
+
 //function to display elements of the queue
-void displayQueue()
-{
+void displayQueue() {
  if(isempty())
   cout<<"Queue is empty\n";
- else
- {
+ else {
   for( int i=front ; i<= rear ; i++)
    cout<<A[i]<<" ";
   cout<<"\n";
  }
 }
 
-int main()
-{
+int main() {
  //inserting elements in queue
  cout<<"Inserting elements in queue\n";
  enqueue(2);
